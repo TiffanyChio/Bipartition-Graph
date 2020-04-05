1e9526b74f824096fb6884b44ef8a13737346e32
tiffanychio <tchio@indeed.com>
2020-04-05 01:14:57 -0500

    write up

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,37 @@
+# Disclaimer: I looked up Leetcode's answer to learn from the best method after failing to come up with an answer. 
+# And added some minor tweaks.
 
-def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+def possible_bipartition(graph)
+  # holds "color" per node
+  # 1 and -1 represent the two groups
+  colors = []
+    
+  graph.length.times do |i|
+    if colors[i].nil?
+      # doesn't matter what you assign to start
+      # if the node has not been touched by the recursion below,
+      # then there are node (clusters) completely separate with no connections
+      colors[i] = 1
+      # dfs checks whether adjacent nodes alternate color
+      return false if !dfs(graph, i, colors)
+    end
+  end
+    
+  return true
+end
+
+def dfs(graph, i, colors)
+  # iterates through neighbors
+  graph[i].each do |neighbor|
+    # if neighbor is not assigned a color, assign it the opposite color
+    if colors[neighbor].nil?
+      colors[neighbor] = -colors[i]
+      return false if !dfs(graph, neighbor, colors)
+    # if a color exists, check if it is the opposite color
+    elsif colors[neighbor] == colors[i]
+      return false
+    end 
+  end
+    
+  return true
 end
